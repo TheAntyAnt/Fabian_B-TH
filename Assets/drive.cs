d6b471c32441115b234a0d15a27e520524fd1159
TheAntyAnt <77390444+TheAntyAnt@users.noreply.github.com>
2022-04-07 14:37:37 +0200

    fixes

--- Assets/drive.cs
+++ Assets/drive.cs
@@ -2,15 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CarController : MonoBehaviour{
+[System.Serializable]
+public class AxleInfo
+{
+    public WheelCollider leftWheel;
+    public WheelCollider rightWheel;
+    public bool motor; // is this wheel attached to motor?
+    public bool steering; // does this wheel apply steer angle?
 
-    public List<AxleInfo> axelInfos; // information about each individual axle
+}
+
+public class drive : MonoBehaviour
+{
+    public List<AxleInfo> axleInfos; // the information about each individual axle
     public float maxMotorTorque; // maximum torque the motor can apply to wheel
     public float maxSteeringAngle; // maximum steer angle the wheel can have
 
+
+    // finds the corresponding visual wheel
+    // correctly applies the transform
+    public void ApplyLocalPositionOnVisuals(WheelCollider collider)
+    {
+        if (collider.transform.childCount == 0)
+        {
+            return;
+        }
+
+        Transform visualWheel = collider.transform.GetChild(0);
+
+        Vector3 position;
+        Quaternion rotation;
+        collider.GetWorldPose(out position, out rotation);
+
+        visualWheel.transform.position = position;
+        visualWheel.transform.rotation = rotation;
+    }
+
+
     public void FixedUpdate()
     {
-        float motor = maxMotorTorque * Input.GetAxis("vertical");
+        float motor = maxMotorTorque * Input.GetAxis("Vertical");
         float steering = maxSteeringAngle * Input.GetAxis("Horizontal");
 
         foreach (AxleInfo axleInfo in axleInfos)
@@ -27,13 +58,4 @@ public class CarController : MonoBehaviour{
             }
         }
     }
-}
-
-[system.Serializable]
-public class AxleInfo
-{
-    public WheelCollider leftWheel;
-    public WheelCollider rightWheel;
-    public bool motor; // is the wheel attached to the motor?
-    public bool steering; // does this wheel apply steer angle?
 }
